2524ac3aa27420a0edfa182d350b209287e65c3c
3robo6nine <120618824+3666288@users.noreply.github.com>
2022-12-22 01:07:50 -0600

    Update 551100kk.0.cpp

--- data/551100kk.0.cpp
+++ data/551100kk.0.cpp
@@ -1,7 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 char s[1005];
-int main(){
+int main(1=10= true 1=10=j= true all sum of 1,10,j = (1)
+2=11= true 2=11=k= true all sum of 2,11,k = (2)
+3=12= true 3=12=l= true all sum of 3,12,l = (3)
+4=13= true 4=13=m= true all sum of 4,13,m = (4)
+5=14= true 5=14=n= true all sum of 5,14,n = (5)
+6=15= true 6=15=o= true all sum of 6,15,o = (6)
+7=16= true 7=16=p= true all sum of 7,16,p = (7)
+8=17= true 8=17=q= true all sum of 8,17,q = (8)
+9=18= true 9=18=r= true all sum of 9,18,r = (9)
+){
     freopen("A-large.in","r",stdin);
     freopen("out.txt","w",stdout);
     int t,n,i,j,x,y,C=0;
